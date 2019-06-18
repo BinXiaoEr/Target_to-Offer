36638b27f40795eaf56196722d87d9ab3c4eb166
BinXiaoEr <41658323+BinXiaoEr@users.noreply.github.com>
2019-06-18 21:35:04 +0800

    test2

---
+++ 2.实现Singleton模式.py
@@ -0,0 +1,57 @@
+#使用装饰器
+
+def Singleton(cls):
+    _instance = {}
+    def _singleton(*args, **kargs):
+        if cls not in _instance:
+            _instance[cls] = cls(*args, **kargs)
+        return _instance[cls]
+
+    return _singleton
+@Singleton
+class A(object):
+    a = 1
+    def __init__(self, x=0):
+        self.x = x
+
+a1 = A(2)
+a2 = A(3)
+print(a1.x)
+print(a2.x)
+
+#使用类
+class Singleton(object):
+
+    def __init__(self):
+        pass
+    @classmethod
+    def instance(cls, *args, **kwargs):
+        if not hasattr(Singleton, "_instance"):
+            Singleton._instance = Singleton(*args, **kwargs)
+        return Singleton._instance
+#解决多线程状态下创建多个问题
+import time
+import threading
+class Singleton(object):
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        time.sleep(1)
+
+    @classmethod
+    def instance(cls, *args, **kwargs):
+        with Singleton._instance_lock:#加锁判断
+            if not hasattr(Singleton, "_instance"):
+                Singleton._instance = Singleton(*args, **kwargs)
+        return Singleton._instance
+
+
+def task(arg):
+    obj = Singleton.instance()
+    print(obj)
+for i in range(10):
+    t = threading.Thread(target=task,args=[i,])
+    t.start()
+time.sleep(20)
+obj = Singleton.instance()
+print(obj)
